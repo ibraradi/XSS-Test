d5524780fd3e0406a889390024ab1112cd08c50d
Ibrahim Radi <90990083+ibraradi@users.noreply.github.com>
2022-09-27 15:56:19 +0200

    Update raw.js

--- raw.js
+++ raw.js
@@ -3,7 +3,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {    
     if (this.status == 200) {    
     alert(this.responseText);     
-    document.getElementById("demo").innerHTML = this.responseText;    
+    document.getElementById(body).innerHTML = this.responseText;    
     }  
 };  
 xhttp.open("GET", "https://accounts.shutterfly.com/sso/v2/tokens", true);  
